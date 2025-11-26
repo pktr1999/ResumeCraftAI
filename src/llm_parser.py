d2a7998640325ce6725794855bae2bc43e3f242e
Prasanna Kumar T R <49908696+pktr1999@users.noreply.github.com>
2025-11-26 17:16:14 +0530

    Update llm_parser.py

--- src/llm_parser.py
+++ src/llm_parser.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="models/gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=os.getenv("GEMINI_API_KEY"),
     temperature=0
 )
 
